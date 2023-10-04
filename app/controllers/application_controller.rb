258a121838a3e4208a23abe04257b4b088d41479
dngst <dngst@protonmail.com>
2023-10-04 12:50:01 +0300

    Show user profile after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,9 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation, tenant_attributes: [:unit_number, :unit_type, :moved_in, :next_payment, :amount_due]])
-    end
+  end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  get '/users/:id', to: 'users#show',as: :user
   # Defines the root path route ("/")
   root "home#index"
 end
