0f51da891f22de4f106e1cf5e0dac1c27ca7d18c
dngst <dngst@protonmail.com>
2023-10-04 13:27:57 +0300

    Annotate models

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,2 +1,24 @@
+# == Schema Information
+#
+# Table name: tenants
+#
+#  id           :bigint           not null, primary key
+#  amount_due   :integer
+#  moved_in     :date
+#  next_payment :date
+#  unit_number  :string
+#  unit_type    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_tenants_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Tenant < ApplicationRecord
 end
--- spec/models/tenant_spec.rb
+++ spec/models/tenant_spec.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: tenants
+#
+#  id           :bigint           not null, primary key
+#  amount_due   :integer
+#  moved_in     :date
+#  next_payment :date
+#  unit_number  :string
+#  unit_type    :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_tenants_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Tenant, type: :model do
