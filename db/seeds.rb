6b01f4bae75c0c7d79aa6725cd0bf0db628b6186
dngst <dngst@protonmail.com>
2023-10-05 20:02:42 +0300

    Add seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,51 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+john = User.create!(
+  fname: "John",
+  lname: "Doe",
+  phone_number: "0715948650",
+  email: "john.doe@example.com",
+  password: "password",
+)
+
+Tenant.create!(
+  user_id: john.id,
+  unit_number: "B30",
+  unit_type: "1 Bedroom",
+  moved_in: "15-11-2022",
+  next_payment: "15-12-2022",
+  amount_due: "10000"
+)
+
+jane = User.create!(
+  fname: "Jane",
+  lname: "Doe",
+  phone_number: "0715948650",
+  email: "jane.doe@example.com",
+  password: "password",
+)
+
+Tenant.create!(
+  user_id: jane.id,
+  unit_number: "B31",
+  unit_type: "1 Bedroom",
+  moved_in: "15-11-2022",
+  next_payment: "15-12-2022",
+  amount_due: "10000"
+)
+
+admin = User.create!(
+  fname: "Erlich",
+  lname: "Bachman",
+  phone_number: "0715948650",
+  email: "elrich@bachmanity.com",
+  password: "password",
+  admin: true
+)
+
+Tenant.create!(
+  user_id: admin.id,
+  unit_number: "0",
+  unit_type: "0",
+  moved_in: "15-11-2012",
+  next_payment: "15-11-2012",
+  amount_due: "0",
+)
