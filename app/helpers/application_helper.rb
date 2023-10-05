3a1816c87fdbd4361003b496e417c8a8ba297930
dngst <dngst@protonmail.com>
2023-10-05 20:55:17 +0300

    Format dates and currency

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def ordinal(number)
+    if (11..13).cover?(number % 100)
+      "#{number}th"
+    else
+      case number % 10
+      when 1
+        "#{number}st"
+      when 2
+        "#{number}nd"
+      when 3
+        "#{number}rd"
+      else
+        "#{number}th"
+      end
+    end
+  end
 end
