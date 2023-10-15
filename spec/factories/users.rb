f694cd15e66556577db40bedcf10d0851d67e7b0
dngst <dngst@protonmail.com>
2023-10-15 16:51:41 +0300

    Add user factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,21 @@
+require 'faker'
+
+FactoryBot.define do
+  factory :user do
+    admin { false }
+    email { Faker::Internet.email  }
+    password { Faker::Internet.password }
+    fname { Faker::Internet.username }
+    lname { Faker::Internet.username }
+    phone_number { Faker::PhoneNumber.cell_phone }
+  end
+
+  factory :admin, class: 'User' do
+    admin { true }
+    email { Faker::Internet.email  }
+    password { Faker::Internet.password }
+    fname { Faker::Internet.username }
+    lname { Faker::Internet.username }
+    phone_number { Faker::PhoneNumber.cell_phone }
+  end
+end
