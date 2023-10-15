41158c6f3eeadc724d35536a60c0be37b5af6fbb
dngst <dngst@protonmail.com>
2023-10-15 14:05:11 +0300

    Fix typo

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: email_address_with_name('<eddieatse@gmail.com', 'Floston Notifications')
+  default from: email_address_with_name('eddieatse@gmail.com', 'Floston Notifications')
   layout 'mailer'
 end
