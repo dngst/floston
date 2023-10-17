f97b499a05592c70b9c75a21f2b5ae9fd9a68331
dngst <dngst@protonmail.com>
2023-10-18 01:33:34 +0300

    Rubocop indentation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,16 +4,17 @@ class SearchController < ApplicationController
   def index
     q = params[:q]
     @requests_list = if current_user&.admin?
-                  Request.joins(:user).where(
-                                             users: { admin_id: current_user.id }).order(created_at: :desc)
-                else
-                  Request.where(user_id: current_user.id).order(created_at: :desc)
-                end
+                       Request.joins(:user).where(
+                         users: { admin_id: current_user.id }
+                       ).order(created_at: :desc)
+                     else
+                       Request.where(user_id: current_user.id).order(created_at: :desc)
+                     end
     @articles_list = if current_user&.admin?
-                  Article.where(admin_id: current_user.id).order(created_at: :desc)
-                else
-                  Article.where(admin_id: current_user.admin_id).order(created_at: :desc)
-                end
+                       Article.where(admin_id: current_user.id).order(created_at: :desc)
+                     else
+                       Article.where(admin_id: current_user.admin_id).order(created_at: :desc)
+                     end
 
     @users_count = User.where(admin_id: current_user.id).ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true)
     @requests_count = @requests_list.ransack(title_or_description_cont: q).result(distinct: true)
