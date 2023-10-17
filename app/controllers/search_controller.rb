02b36b2254a7ab864cb710539ae1f42327ee03fa
dngst <dngst@protonmail.com>
2023-10-17 22:48:02 +0300

    Paginate search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,8 +3,12 @@ class SearchController < ApplicationController
 
   def index
     q = params[:q]
-    @users = User.ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true)
-    @requests = Request.ransack(title_or_description_or_cont: q).result(distinct: true)
-    @articles = Article.ransack(title_or_body_cont: q).result(distinct: true)
+    @users = User.ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true).page(params[:page])
+    @requests = Request.ransack(title_or_description_or_cont: q).result(distinct: true).page(params[:page])
+    @articles = Article.ransack(title_or_body_cont: q).result(distinct: true).page(params[:page])
+
+    @users_count = User.ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true)
+    @requests_count = Request.ransack(title_or_description_or_cont: q).result(distinct: true)
+    @articles_count = Article.ransack(title_or_body_cont: q).result(distinct: true)
   end
 end
