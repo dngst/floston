2137431893faa1c65c2ee05e78052fdc68052ced
dngst <dngst@protonmail.com>
2023-10-18 01:28:01 +0300

    Fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,12 +3,24 @@ class SearchController < ApplicationController
 
   def index
     q = params[:q]
-    @users = User.ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true).page(params[:page])
-    @requests = Request.ransack(title_or_description_or_cont: q).result(distinct: true).page(params[:page])
-    @articles = Article.ransack(title_or_body_cont: q).result(distinct: true).page(params[:page])
+    @requests_list = if current_user&.admin?
+                  Request.joins(:user).where(
+                                             users: { admin_id: current_user.id }).order(created_at: :desc)
+                else
+                  Request.where(user_id: current_user.id).order(created_at: :desc)
+                end
+    @articles_list = if current_user&.admin?
+                  Article.where(admin_id: current_user.id).order(created_at: :desc)
+                else
+                  Article.where(admin_id: current_user.admin_id).order(created_at: :desc)
+                end
 
-    @users_count = User.ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true)
-    @requests_count = Request.ransack(title_or_description_or_cont: q).result(distinct: true)
-    @articles_count = Article.ransack(title_or_body_cont: q).result(distinct: true)
+    @users_count = User.where(admin_id: current_user.id).ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true)
+    @requests_count = @requests_list.ransack(title_or_description_cont: q).result(distinct: true)
+    @articles_count = @articles_list.ransack(title_or_body_cont: q).result(distinct: true)
+
+    @users = User.where(admin_id: current_user.id).ransack(fname_or_lname_or_email_or_phone_number_cont: q).result(distinct: true).page(params[:page])
+    @requests = @requests_list.ransack(title_or_description_cont: q).result(distinct: true).page(params[:page])
+    @articles = @articles_list.ransack(title_or_body_cont: q).result(distinct: true).page(params[:page])
   end
 end
