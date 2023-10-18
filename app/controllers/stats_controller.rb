8b1f52ce28cf72539631af9e63ac85c0fd0a475a
dngst <dngst@protonmail.com>
2023-10-19 02:11:47 +0300

    Authorize stats controller

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,4 +1,8 @@
 class StatsController < ApplicationController
+  include RequireAdmin
+
+  before_action :authenticate_user!, :require_admin
+
   def index
     @user = User.friendly.find(params[:user_id])
   end
