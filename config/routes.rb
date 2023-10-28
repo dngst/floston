03ae34ece0f8d85a5ae9dc9bc8096fbdda41db57
dngst <dngst@protonmail.com>
2023-10-29 00:46:09 +0300

    Add floston color

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'search', to: 'search#index'
+  get 'features', to: 'features#index'
   resources :articles
   devise_for :users, controllers: { registrations: 'registrations' }
   resources :users do
