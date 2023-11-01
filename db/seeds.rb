1709a825da01a52f7b85c573c7b58d6eb3ec1d04
dngst <dngst@protonmail.com>
2023-11-01 16:53:02 +0300

    Edit seeds to use find_or_create_by!

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,48 @@
 require 'faker'
 
-elrich = User.create!(
+elrich = User.find_or_create_by!(
   fname: "Erlich",
   lname: "Bachman",
   phone_number: "0715948650",
   email: "erlich@bachmanity.com",
-  password: "password",
   admin: true
-)
+) do |user|
+  user.password = "password"
+end
 
-richard = User.create!(
+richard = User.find_or_create_by!(
   fname: "Richard",
   lname: "Hendrix",
   phone_number: "0715948650",
   email: "richy@piedpipper.com",
-  password: "password",
   admin: true
-)
+) do |user|
+  user.password = "password"
+end
 
 50.times do
-  Property.create!(
+  Property.find_or_create_by!(
     name: Faker::Address.street_name,
     user_id: elrich.id
   )
 end
 
-richards_property = Property.create!(
+richards_property = Property.find_or_create_by!(
   name: "Viraj, Laikipia Road",
   user_id: richard.id
 )
 
-jackie = User.create!(
+jackie = User.find_or_create_by!(
   fname: "Jackie",
   lname: "Brown",
   phone_number: "0715948650",
   email: "jackie@jackiebrown.com",
-  password: "password",
   admin_id: richard.id
-)
+) do |user|
+  user.password = "password"
+end
 
-Tenant.create!(
+Tenant.find_or_create_by!(
   user_id: jackie.id,
   unit_number: "B29",
   unit_type: "1 Bedroom",
@@ -49,16 +52,17 @@ Tenant.create!(
   property_id: richards_property.id
 )
 
-john = User.create!(
+john = User.find_or_create_by!(
   fname: "John",
   lname: "Doe",
   phone_number: "0715948650",
   email: "john.doe@example.com",
-  password: "password",
   admin_id: elrich.id
-)
+) do |user|
+  user.password = "password"
+end
 
-Tenant.create!(
+Tenant.find_or_create_by!(
   user_id: john.id,
   unit_number: "B30",
   unit_type: "1 Bedroom",
@@ -68,16 +72,17 @@ Tenant.create!(
   property_id: 10
 )
 
-jane = User.create!(
+jane = User.find_or_create_by!(
   fname: "Jane",
   lname: "Doe",
   phone_number: "0715948650",
   email: "jane.doe@example.com",
-  password: "password",
   admin_id: elrich.id
-)
+) do |user|
+  user.password = "password"
+end
 
-Tenant.create!(
+Tenant.find_or_create_by!(
   user_id: jane.id,
   unit_number: "B31",
   unit_type: "1 Bedroom",
@@ -87,7 +92,7 @@ Tenant.create!(
   property_id: 10
 )
 
-Article.create!(
+Article.find_or_create_by!(
   title: "Recent change of management",
   body: "Following a recent incident, some changes have been made...",
   user_id: elrich.id,
@@ -95,7 +100,7 @@ Article.create!(
   property_id: 10
 )
 
-Article.create!(
+Article.find_or_create_by!(
   title: "Key people at Argon Properties",
   body: "Some of the names and contacts of the people involved in this project...",
   user_id: elrich.id,
@@ -103,7 +108,7 @@ Article.create!(
   property_id: 10
 )
 
-Article.create!(
+Article.find_or_create_by!(
   title: "Faulty tap at the parking lot near block D",
   body: "There's a licking tap near block D at the parking lot. Please refrain
    from using it while we organise for repairs to be made.",
@@ -111,7 +116,7 @@ Article.create!(
   property_id: 10
 )
 
-Article.create!(
+Article.find_or_create_by!(
   title: "Richard can also write some articles like this one",
   body: "Jobs or... Jobs",
   user_id: richard.id,
@@ -119,42 +124,43 @@ Article.create!(
   property_id: 10
 )
 
-Request.create!(
+Request.find_or_create_by!(
   title: "Window repair",
   description: "Woke up this morning to find a window pane in my bedroom had fallen down",
   user_id: jane.id
 )
 
-Request.create!(
+Request.find_or_create_by!(
   title: "Disturbance",
   description: "The tenant at door no. 2E is constatly playing loud music since they moved in and is rude when asked to tone it down.",
   user_id: jane.id
 )
 
-Request.create!(
+Request.find_or_create_by!(
   title: "Key replacement",
   description: "I lost my gate key. Could you please organise for me another one?",
   user_id: john.id
 )
 
 15.times do
-  Reminder.create!(
+  Reminder.find_or_create_by!(
     amount: "62104",
     user_id: jane.id
   )
 end
 
 50.times do
-  user = User.create!(
+  user = User.find_or_create_by!(
     fname: Faker::Internet.username,
     lname: Faker::Internet.username,
     phone_number: Faker::PhoneNumber.cell_phone,
     email: Faker::Internet.email,
-    password: "password",
     admin_id: elrich.id
-  )
+  ) do |user|
+    user.password = "password"
+  end
 
-  Tenant.create!(
+  Tenant.find_or_create_by!(
     user_id: user.id,
     unit_number: Faker::Number.unique.number(digits: 3),
     unit_type: "1 Bedroom",
@@ -167,7 +173,7 @@ end
 
 
 50.times do
-  Request.create!(
+  Request.find_or_create_by!(
     title: Faker::Lorem.words(number: 5).join(' '),
     description: Faker::Lorem.paragraphs(number: 5, supplemental: true).join("\n\n"),
     user_id: jane.id
@@ -175,7 +181,7 @@ end
 end
 
 50.times do
-  Article.create!(
+  Article.find_or_create_by!(
     title: Faker::Lorem.words(number: 7).join(' '),
     body:  Faker::Lorem.paragraphs(number: 5, supplemental: true).join("\n\n"),
     user_id: elrich.id,
