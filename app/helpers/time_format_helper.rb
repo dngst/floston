25cc78762372b6cedd64e43cf2c87961c11647d8
dngst <dngst@protonmail.com>
2023-11-14 06:11:53 +0300

    Lint Use implicit return Use Time.zone

--- app/helpers/time_format_helper.rb
+++ app/helpers/time_format_helper.rb
@@ -1,5 +1,5 @@
 module TimeFormatHelper
   def subscription_time(date_time)
-    Time.parse(date_time).strftime("%a, %b %d, %Y %H:%M:%S")
+    Time.zone.parse(date_time).strftime('%a, %b %d, %Y %H:%M:%S')
   end
 end
