989a526d5c20b0326515c9c25949311a204e5aae
dngst <dngst@protonmail.com>
2023-11-21 20:26:09 +0300

    Use find_each

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -32,7 +32,7 @@ class Tenant < ApplicationRecord
 
   def self.send_due_date_reminders
     tenants_to_remind = where('next_payment <= ?', 5.days.from_now)
-    tenants_to_remind.each do |tenant|
+    tenants_to_remind.find_each do |tenant|
       Reminder.create!(amount: tenant.amount_due, user_id: tenant.user.id) if PaymentDueMailer.reminder_email(
         tenant.user, tenant
       ).deliver_now
