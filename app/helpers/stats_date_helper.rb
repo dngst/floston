3a7766e7b9bbf2e68c5c1a0173b899d835d33b34
dngst <dngst@protonmail.com>
2023-12-06 01:23:44 +0300

    Use safe navigation (&.)

--- app/helpers/stats_date_helper.rb
+++ app/helpers/stats_date_helper.rb
@@ -1,5 +1,5 @@
 module StatsDateHelper
   def format_stats_datetime(datetime)
-    datetime.strftime("%a, #{datetime.day} %b %Y at %I:%M %p") if datetime
+    datetime&.strftime("%a, #{datetime.day} %b %Y at %I:%M %p")
   end
 end
