65978dbd4d0146d4c72fca831d96d60392c61e36
dngst <dngst@protonmail.com>
2023-12-14 22:52:47 +0300

    Eager load user for each comment

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -31,7 +31,7 @@ class RequestsController < ApplicationController
 
   # GET /requests/1 or /requests/1.json
   def show
-    @request = Request.includes(comments: :user).friendly.find(params[:id])
+    @request = Request.friendly.find(params[:id])
     @user =  @request.user
     @comment_count ||= @request.comments.length
     @comment = @request.comments.build(user: @user)
