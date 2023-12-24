b70b788ff1d7f43ba9229f2a5defd067723ca968
dngst <dngst@protonmail.com>
2023-12-25 02:58:53 +0300

    Show flash messages

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -65,12 +65,19 @@ class PropertiesController < ApplicationController
 
   # DELETE /properties/1 or /properties/1.json
   def destroy
+    flash.now[:notice] = t('properties.deleted')
     @property.destroy
 
     Rails.cache.delete('property_ids')
 
     respond_to do |format|
       format.html { redirect_to properties_url }
+      format.turbo_stream do
+        render turbo_stream: [
+          turbo_stream.remove(@property),
+          turbo_stream.replace('flash-messages', partial: 'layouts/flash')
+        ]
+      end
     end
   end
 
