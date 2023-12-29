174773b7ab8f24b5414078fd901a464a13c893fa
dngst <dngst@protonmail.com>
2023-12-29 22:12:46 +0300

    Set alignment

--- app/helpers/article_date_helper.rb
+++ app/helpers/article_date_helper.rb
@@ -2,6 +2,6 @@ module ArticleDateHelper
   def display_timestamp(article)
     return unless article.created_at != article.updated_at
 
-    "- Updated on #{article.updated_at.strftime('%b %d, %Y')}"
+    "Updated on #{article.updated_at.strftime('%b %d, %Y')}"
   end
 end
