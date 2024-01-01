2e12d7963b380056f26acf458ccbc333172eb9f3
dngst <dngst@protonmail.com>
2024-01-01 22:59:54 +0300

    Hide scroll to top

--- app/javascript/controllers/back_to_top_controller.js
+++ app/javascript/controllers/back_to_top_controller.js
@@ -1,7 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  connect() {
+    this.setupScrollListener()
+    this.hideButton()
+  }
+
+  setupScrollListener() {
+    window.addEventListener('scroll', () => this.scrollFunction())
+  }
+
+  scrollFunction() {
+    const mybutton = document.getElementById("scrollToTop")
+
+    if (window.scrollY > 600) {
+      mybutton.style.display = "block"
+    } else {
+      mybutton.style.display = "none"
+    }
+  }
+
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: "smooth" })
   }
+
+  hideButton() {
+    const mybutton = document.getElementById("scrollToTop")
+    mybutton.style.display = "none"
+  }
+
+  disconnect() {
+     window.removeEventListener('scroll', () => this.scrollFunction())
+   }
 }
