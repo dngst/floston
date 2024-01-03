b4fc50c18998a7c2a728585e80a545b04974cbb8
dngst <dngst@protonmail.com>
2024-01-03 12:22:41 +0300

    Fix: body parameter not available

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,11 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
-  connect() {
-    this.element.addEventListener("submit", this.clear.bind(this))
-  }
-
   clear() {
     this.element.reset()
   }
