857ade7c433c845cce731611aca3c0410751d1bc
dngst <dngst@protonmail.com>
2024-02-17 00:32:40 +0300

    Set homepage

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,12 @@
 
 class HomeController < ApplicationController
   def index
-    @home_page = true
     if current_user&.admin?
       redirect_to users_path
-    elsif current_user.present?
+    elsif current_user
       redirect_to user_path(current_user)
+    else
+      @home_page = true
     end
   end
 end
