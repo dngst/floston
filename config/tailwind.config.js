fb1573abab51e8b61c76aeac0ffeaa533d4a61f1
dngst <dngst@protonmail.com>
2024-05-28 19:51:12 +0300

    Set width

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -9,6 +9,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+        maxWidth: {
+            '1/2': '65%',
+        },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
