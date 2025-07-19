0580afd3aa1ec1f71a54e7deaa84143cf6f88fcf
dngst <94643104+dngst@users.noreply.github.com>
2025-07-20 02:47:46 +0300

    Remove annotate (#35)

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,20 +12,3 @@ class Comment < ApplicationRecord
     %w[body]
   end
 end
-
-# == Schema Information
-#
-# Table name: comments
-#
-#  id         :integer          not null, primary key
-#  body       :text             not null
-#  request_id :integer          not null
-#  user_id    :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_comments_on_request_id  (request_id)
-#  index_comments_on_user_id     (user_id)
-#
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,20 +7,3 @@ RSpec.describe Comment do
   it { is_expected.to belong_to :user }
   it { is_expected.to belong_to :request }
 end
-
-# == Schema Information
-#
-# Table name: comments
-#
-#  id         :integer          not null, primary key
-#  body       :text             not null
-#  request_id :integer          not null
-#  user_id    :integer          not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#
-# Indexes
-#
-#  index_comments_on_request_id  (request_id)
-#  index_comments_on_user_id     (user_id)
-#
