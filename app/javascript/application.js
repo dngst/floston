a9467152557733b4e7cc392bd0f4040df461444c
dngst <dngst@protonmail.com>
2026-03-31 16:39:17 +0300

    Fix setConfirmMethod deprecation

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,14 +3,14 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "custom/companion"
 
-Turbo.setConfirmMethod((message, element) => {
-  let dialog = document.getElementById("turbo-confirm")
-  dialog.querySelector("p").textContent = message
-  dialog.showModal()
+Turbo.config.forms.confirm = (message, element) => {
+    let dialog = document.getElementById("turbo-confirm")
+    dialog.querySelector("p").textContent = message
+    dialog.showModal()
 
-  return new Promise((resolve, reject) => {
-    dialog.addEventListener("close", () => {
-      resolve(dialog.returnValue == "confirm")
-    }, { once: true })
-  })
-})
+    return new Promise((resolve, reject) => {
+        dialog.addEventListener("close", () => {
+            resolve(dialog.returnValue == "confirm")
+        }, { once: true })
+    })
+}
